dbf6d2c68c3ed1e55956958109e214087f1f17eb
Diego-Rodriguez5 <a.22308051281296@cbtis128.edu.mx>
2024-09-11 07:49:50 -0600

    Add files via upload

---
+++ mi_conjunto.py
@@ -0,0 +1,6 @@
+print("Ejemplo conjuntos set:")
+tipodeportes={"Futbol","Tenis","Basketbol"}
+print(tipodeportes)
+print("Lista de elementos del conjunto tipodeportes loop for")
+for diego in tipodeportes:
+    print(diego)
---
+++ mi_diccionario.py
@@ -0,0 +1,10 @@
+print("Ejemplo: Mi diccionario")
+diego = {
+    "brand": "Ford",
+    "model": "Mustang",
+    year": 1964
+}
+print(diego)
+
+for x in diego:
+    print(x)
